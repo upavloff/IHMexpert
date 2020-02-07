a4f84b70e96295354c05310eb3b401b95e278723
ulrych8 <pavloffulysse@gmail.com>
2020-02-07 02:21:08 +0100

    cors issue 6 au moins

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at ${port}...`));
+
+/*app.use(cors);*/
 app.use(express.static('./'));
-app.use(cors);
 //to parse incomming data as json
 app.use(express.json( /*{limit:'1mb'} */ ));
 
