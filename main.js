8e7a323744c2c129e7b3fa8b369bf4d387e11686
ulrych8 <pavloffulysse@gmail.com>
2020-02-07 01:32:00 +0100

    cors issue 2

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ async function infoPOSTING( /* mouse event*/ event) {
     let x = event.clientX;
     let y = event.clientY;
     options.body = JSON.stringify({ x: x, y: y });
-    const response = await fetch('/api', options);
+    const response = await fetch('http://localhost:3000/api', options);
     const data = await response.json();
     console.log(data);
 }
